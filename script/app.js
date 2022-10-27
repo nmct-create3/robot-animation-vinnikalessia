62805cea9edea5044a9d0f001941695c74a20d2b
alessia vinnik <alessia.vinnik@student.howest.be>
2022-10-27 17:33:38 +0200

    buttons klaar

--- script/app.js
+++ script/app.js
@@ -1,63 +1,71 @@
 let btnSlow, btnNormal, btnFast, btnPause, btnPlay, btnLight, lightning;
 
-
-const robot = function(){
-    let tl = gsap.timeline({
-        defaults: {
-          duration: 1,
-          ease: 'power1.inOut',
-        },
-        repeat: -1,
-        yoyo: true,
-    })
-    tl.set('#Shadow', {
-        transformOrigin: '50% 100%'
-    })
-    .fromTo('#Robot',{
+const robot = function () {
+  let tl = gsap.timeline({
+    defaults: {
+      duration: 1,
+      ease: 'power1.inOut',
+    },
+    repeat: -1,
+    yoyo: true,
+  });
+  tl.set('#Shadow', {
+    transformOrigin: '50% 100%',
+  })
+    .fromTo(
+      '#Robot',
+      {
         y: 2.5,
-      },{
+      },
+      {
         y: -2.5,
-      })
-      .to('#Shadow',{
+      }
+    )
+    .to(
+      '#Shadow',
+      {
         scale: 0.75,
       },
-        '<',
-    )
-    buttons(tl)
-}
+      '<'
+    );
+  buttons(tl);
+};
 
-const buttons = function(tl){
-    btnSlow.onclick = () => tl.timeScale(.5)
-    btnNormal.onclick = () => tl.timeScale(1)
-    btnFast.onclick = () => tl.timeScale(2)
-    btnPause.onclick = function(){
-        tl.paused(!tl.paused())
-        btnPause.classList.toggle('is-paused')
+const buttons = function (tl) {
+  btnSlow.onclick = () => tl.timeScale(0.5);
+  btnNormal.onclick = () => tl.timeScale(1);
+  btnFast.onclick = () => tl.timeScale(2);
+
+  btnPP.onclick = function () {
+    tl.paused(!tl.paused());
+    if (tl.paused()) {
+      btnPlay.style.display = 'block';
+      btnPause.style.display = 'none';
+    } else {
+      btnPause.style.display = 'block';
+      btnPlay.style.display = 'none';
     }
-}
+  };
+};
 
-const lightningButton = function(){
+const lightningButton = function () {
   btnLight = document.queryselector('.js-lightning');
   lightning = document.getElementById(btnLight);
   btnLight.onclick = () => lightning.beginElement();
-}
+};
 
 document.addEventListener('DOMContentLoaded', function () {
-    btnSlow = document.querySelector('.js-slow');
-    btnNormal = document.querySelector('.js-normal');
-    btnFast = document.querySelector('.js-fast');
+  btnSlow = document.querySelector('.js-slow');
+  btnNormal = document.querySelector('.js-normal');
+  btnFast = document.querySelector('.js-fast');
 
-    btnPlay = document.querySelector('.js-play');
-    btnPause = document.querySelector('.js-pause');
+  btnPlay = document.querySelector('.js-play-icon');
+  btnPause = document.querySelector('.js-pause-icon');
 
-    // btnLight = document.querySelector('.js-light');
+  btnPP = document.querySelector('.js-pause');
 
-    robot();
-    lighteningButton();
+  // btnLight = document.querySelector('.js-light');
+
+  robot();
+  lighteningButton();
 });
-
-
-
-
-
-
